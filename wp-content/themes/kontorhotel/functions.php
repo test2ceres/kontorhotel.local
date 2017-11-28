dc1eec2c0cac93361432ca1908a0daf3fc831ce1
Test Ceres <tran.pham@ceres.com.vn>
2017-11-28 15:29:44 +0700

    add plugin

--- wp-content/themes/kontorhotel/functions.php
+++ wp-content/themes/kontorhotel/functions.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * custom function of kontorhotel template
+*/
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+if ( function_exists('register_sidebar') ) {
+    register_sidebar(array(
+        'name' => 'Left Sidebar',
+        'id' => 'left-sidebar',
+        'description' => 'Appears as the sidebar on the right site',
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widgettitle">',
+        'after_title' => '</h2>',
+    ));
+}
---
+++ wp-content/themes/kontorhotel/sidebar-leftsidebar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for the sidebar containing the main widget area
+ *
+ * @package WordPress
+ * @subpackage kontorhotel
+ * @since Kontor Hotel
+ */
+?>
+<aside id="left-sidebar" class="left-sidebar widget-area" role="complementary">
+    <?php dynamic_sidebar( 'left-sidebar' ); ?>
+</aside><!-- .sidebar .widget-area -->
